8a80ea3b9849a09a64d12d382d56c39c1093e8b6
Ahmedmayahi <80107996+AhmedMh0@users.noreply.github.com>
2023-06-27 14:45:48 +0300

    fix eslint errors

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const hamburgar = document.querySelector(".hamburgar")
-const navmenu = document.querySelector(".nav-menu")
+/* eslint-disable linebreak-style */
+const hamburgar = document.querySelector('.hamburgar');
+const navmenu = document.querySelector('.nav-menu');
+hamburgar.addEventListener('click', () => {
+  hamburgar.classList.toggle('active');
+  navmenu.classList.toggle('active');
+});
 
-hamburgar.addEventListener("click",() => {
-    hamburgar.classList.toggle("active"); 
-    navmenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburgar.classList.remove("active");
-    navmenu.classList.remove("active");
-}))
-
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburgar.classList.remove('active');
+  navmenu.classList.remove('active');
+}));
