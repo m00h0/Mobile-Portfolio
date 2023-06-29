930b79cc1a29265ac712fe4b1ce92aef609b51d7
Ahmedmayahi <80107996+AhmedMh0@users.noreply.github.com>
2023-06-30 00:41:41 +0300

    fix the image

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const projects = [
   {
     id: 1,
     title: 'Multi-Post Stories Gain+Glory',
-    snapshot: 'Images/assets/Snapshoot-Portfolio.png',
+    snapshot: './Images/assets/Snapshoot-Portfolio.svg',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry",
     skills: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
     projectInfo: { company: 'Microverse', role: 'Back END Dev', year: 2015 },
@@ -27,7 +27,7 @@ const projects = [
   {
     id: 2,
     title: 'Multi-Post Stories Gain+Glory',
-    snapshot: 'Images/assets/Snapshoot-Portfolio.png',
+    snapshot: './Images/assets/Snapshoot-Portfolio.svg',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry",
     skills: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
     projectInfo: { company: 'Microverse', role: 'Back END Dev', year: 2015 },
@@ -39,7 +39,7 @@ const projects = [
   {
     id: 3,
     title: 'Multi-Post Stories Gain+Glory',
-    snapshot: 'Images/assets/Snapshoot-Portfolio.png',
+    snapshot: './Images/assets/Snapshoot-Portfolio.svg',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry",
     skills: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
     projectInfo: { company: 'Microverse', role: 'Back END Dev', year: 2015 },
@@ -51,7 +51,7 @@ const projects = [
   {
     id: 4,
     title: 'Multi-Post Stories Gain+Glory',
-    snapshot: 'Images/assets/Snapshoot-Portfolio.png',
+    snapshot: './Images/assets/Snapshoot-Portfolio.svg',
     skills: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
     projectInfo: { company: 'Microverse', role: 'Back END Dev', year: 2015 },
     supdescription: 'Keeping track of hundreds of components',
@@ -63,7 +63,7 @@ const projects = [
   {
     id: 5,
     title: 'Multi-Post Stories Gain+Glory Gain+Glory',
-    snapshot: 'Images/assets/Snapshoot-Portfolio.png',
+    snapshot: './Images/assets/Snapshoot-Portfolio.svg',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry",
     skills: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
     supdescription: 'Keeping track of hundreds of components',
@@ -75,7 +75,7 @@ const projects = [
   {
     id: 6,
     title: 'Multi-Post Stories Gain+Glory',
-    snapshot: 'Images/assets/Snapshoot-Portfolio.png',
+    snapshot: './Images/assets/Snapshoot-Portfolio.svg',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry",
     skills: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
     supdescription: 'Keeping track of hundreds of components',
